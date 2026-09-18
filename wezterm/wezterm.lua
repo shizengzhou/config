490cb91990e17afeade42c9a83033c7d3044b8d8
szz <shizengzhou@outlook.com>
2026-09-18 12:46:13 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,14 +4,11 @@ local config = {
   font_size = 13,
   color_scheme = 'Catppuccin Mocha',
   use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
   adjust_window_size_when_changing_font_size = false,
   enable_scroll_bar = true,
   default_prog = { 'powershell.exe', '-NoLogo' },
   scrollback_lines = 10000,
-}
-
-config.keys = {
-  { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
 }
 
 wezterm.on('gui-startup', function (cmd)
